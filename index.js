20352e27e5cac527f8bcb91a0dfd9354ba413163
Corentin Deleuse <dlse.corentin@gmail.com>
2022-12-18 08:15:00 +0100

    db config

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome" });
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}/`)
+})
